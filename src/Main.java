2a9ccb7312f75eae9509a7b37052da772b2a0753
Tarasov Sergei <www.neon@mail.ru>
2022-08-04 09:23:00 +0500

    решил дополнительное задание

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task6();
+    }
+
+    private static void taskStartMessage(String taskNumber) {
+        System.out.println("Task " + taskNumber);
+    }
+
+    private static void taskFinishMessage() {
+        System.out.println();
     }
 
     /** Task 1
@@ -14,11 +25,11 @@ public class Main {
      * Если год не високосный, то, соответственно: “номер года — не високосный год”.
      */
     private static void task1() {
-        System.out.println("Task 1");
+        taskStartMessage("1");
         int year = 2022;
         String yearStatus = checkYear(year);
         printStatus(year, yearStatus);
-        System.out.println();
+        taskFinishMessage();
     }
 
     private static void printStatus(int year, String yearStatus) {
@@ -47,13 +58,13 @@ public class Main {
      * и для какой ОС (Android или iOS) нужно установить пользователю.
      */
     private static void task2() {
-        System.out.println("Task 2");
+        taskStartMessage("2");
         int osType = 1; // 0 - iOS, 1 - Android
         int yearOfManufacture = 2015;
         String osName = getOsName(osType);
         String message = getMessage(osName, yearOfManufacture);
         System.out.println(message);
-        System.out.println();
+        taskFinishMessage();
     }
 
     private static String getOsName(int osType) {
@@ -82,15 +93,15 @@ public class Main {
      *      количество дней доставки.
      */
     private static void task3() {
-        System.out.println("Task 3");
+        taskStartMessage("3");
         int deliveryDistance = 90;
         int requiredNumberOfDays = getRequiredNumberOfDays(deliveryDistance);
         System.out.println("Потребуется дней: " + requiredNumberOfDays);
-        System.out.println();
+        taskFinishMessage();
     }
 
     private static int getRequiredNumberOfDays(int deliveryDistance) {
-        int deliveryDays = 0;
+        int deliveryDays;
         int minDistance = 20;
         int stepDistance = 40;
         if (deliveryDistance < 0 || deliveryDistance > 20000) {
@@ -100,4 +111,64 @@ public class Main {
         return deliveryDays;
     }
 
+    /** Task 6*
+     * Снова вспоминаем домашнее задание по массивам. В нем была задача, которая требовала высчитать среднюю выплату за день.
+     * Был дан сгенерированный массив из 30 значений от 100 до 200 тысяч, для его генерации допускается использовать метод
+     *      из прошлого домашнего задания.
+     * Нам нужно понять, какую в среднем сумму наша компания тратила в течение данных 30 дней.
+     * Нужно написать программу, которая посчитает среднее значение трат за месяц (то есть сумму всех трат за месяц поделить
+     *      на количество дней), и вывести в консоль результат в формате: «Средняя сумма трат за месяц составила … рублей».
+     * Важно помнить: подсчет среднего значения может иметь остаток (то есть быть не целым, а дробным числом).
+     * Нужно сгенерировать массив, подать его в наш метод, а внутри метода подсчитать сумму элементов и вычислить среднее значение,
+     *      которое нужно вернуть из метода в виде результата.
+     * Сложность в том, что метод нужно не просто написать, но еще и декомпозировать.
+     * То есть для работы этого метода нужно будет создать еще методы (1 или более), которые его будут обслуживать и вычислять
+     *      промежуточные результаты. Среднее значение нужно вычислять в дробном виде, так как результат должен быть точным.
+     */
+    private static void task6() {
+        taskStartMessage("6*");
+        int listLength = 30;
+        int[] expensesList = generateExpensesList(listLength);
+        printExpensesList(expensesList);
+        double averageExpenses = getAverageExpenses(expensesList);
+        printAverageExpensesReport(averageExpenses);
+        taskFinishMessage();
+    }
+
+    private static void printAverageExpensesReport(double averageExpenses) {
+        System.out.println("Средняя сумма трат за месяц составила " + averageExpenses + " рублей");
+    }
+
+    private static void printExpensesList(int[] expensesList) {
+        System.out.println("Значения массива: " + Arrays.toString(expensesList));
+    }
+
+    private static double getAverageExpenses(int[] expensesList) {
+        int amountExpenses = getAmountExpenses(expensesList);
+        return (double) amountExpenses / expensesList.length;
+    }
+
+    private static int getAmountExpenses(int[] expensesList) {
+        int amountExpenses = 0;
+        for (int currExpenses : expensesList) {
+            amountExpenses += currExpenses;
+        }
+        return amountExpenses;
+    }
+
+    private static int[] generateExpensesList(int listLength) {
+        int[] expensesList = new int[listLength];
+        for (int i = 0; i < expensesList.length; i++) {
+            expensesList[i] = generateExpensesPerDay();
+        }
+        return expensesList;
+    }
+
+    private static int generateExpensesPerDay() {
+        Random random = new Random();
+        return random.nextInt(100_000) + 100_000;
+    }
+
+
 }
+
